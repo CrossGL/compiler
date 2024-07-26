20a329737e9b387fc3fc0b5004b3420b6dad59c8
nomaannm <nomanm2324@gmail.com>
2024-07-26 14:40:57 +0530

    Added support for more keywords, also changed "self" to "XGLUtils".

--- lib/Lexer/lexer.cpp
+++ lib/Lexer/lexer.cpp
@@ -1,13 +1,14 @@
 #include "lexer.h"
 
-namespace self {
+namespace XGLUtils {
   // For better documentation
   bool isaplhanum(char c) { return isdigit(c) or isalpha(c); }
   bool isKeywordCheck(const std::string& string) {
     if(string == "int" || string == "float" || string == "bool" ||
        string == "vec2" || string == "vec3" || string == "vec4" ||
        string == "mat2" || string == "mat3" || string == "mat4" ||
-       string == "sampler2D" || string == "sampler3D" || string == "shader"
+       string == "sampler2D" || string == "sampler3D" || string == "shader" ||
+       string == "void" || string == "return"
        )
       return true;
     return false;
@@ -38,26 +39,26 @@ void Lexer::skipWhitespace() {
 }
 
 Token Lexer::number() {
-  std::string num = "";
+  std::string num;
 
   do {
     num += currentChar();
     advance();
   } while (isdigit(currentChar()));
 
-  return Token(TokenType::NUMBER, num);
+  return {TokenType::NUMBER, num};
 }
 
 
 Token Lexer::identifier() {
-  std::string vname = "";
+  std::string vname;
   do {
     vname += currentChar();
     advance();
-  } while (self::isaplhanum(
+  } while (XGLUtils::isaplhanum(
       currentChar()) || currentChar() == '_'); // Underscore support added
-  if(self::isKeywordCheck(vname)) return Token(TokenType::KEYWORD, vname);
-  return Token(TokenType::IDENTIFIER, vname);
+  if(XGLUtils::isKeywordCheck(vname)) return {TokenType::KEYWORD, vname};
+  return {TokenType::IDENTIFIER, vname};
 }
 
 std::vector<Token> Lexer::getTokens() {
@@ -67,7 +68,7 @@ std::vector<Token> Lexer::getTokens() {
     if (currentChar() == ' ')
       skipWhitespace();
     if (currentChar() == '\0')
-      tokens.push_back(Token(TokenType::END_OF_FILE, "\0"));
+      tokens.emplace_back(TokenType::END_OF_FILE, "\0");
     if (isalpha(currentChar()))
       tokens.push_back(identifier());
     if (isdigit(currentChar()))
@@ -75,9 +76,9 @@ std::vector<Token> Lexer::getTokens() {
     if (currentChar() == '+' or currentChar() == '-' or currentChar() == '/' or
         currentChar() == '*' or currentChar() == '=' or currentChar() == ';' or
         currentChar() == '(' or currentChar() == ')' or currentChar() == ',') {
-      std::string s = "";
+      std::string s;
       s += currentChar();
-      tokens.push_back(Token(TokenType::SYMBOL, s));
+      tokens.emplace_back(TokenType::SYMBOL, s);
     }
   } while (advance());
 
