596f143f5291d21cf486d814c0eadc4e1ce67562
nomaannm <nomanm2324@gmail.com>
2024-07-27 11:47:52 +0530

    Namespace 'XGLUtils' defined in its separate translation unit

--- lib/Custom-Namespaces/XGLUtils.cpp
+++ lib/Custom-Namespaces/XGLUtils.cpp
@@ -1,1 +1,15 @@
 #include "XGLUtils.h"
+
+#include <algorithm>
+#include<cctype>
+#include <lexer.h>
+
+inline bool XGLUtils::isaplhanum(char c) {
+    return isdigit(c) or isalpha(c);
+}
+
+bool XGLUtils::isKeywordCheck(const std::string& word){
+    return std::find(keywords.begin(), keywords.end(), word) != keywords.end();
+}
+
+
