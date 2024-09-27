cbeb29eadab6f2edf256242afb8589be47de1d33
Thirulogeswaren <thirulogeswaren.v@outlook.com>
2024-09-27 17:26:17 +0530

    removed absolute paths

--- build.bat
+++ build.bat
@@ -7,13 +7,13 @@ if NOT EXIST %LLVM_BUILD_DIR% (
 	EXIT /B
 )
 
-CALL "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+if NOT EXIST build (
+	MKDIR build
+)
 
-MKDIR build
+cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH=thirdparty\llvm-project\install
 
-cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -DCMAKE_PREFIX_PATH=thirdparty\llvm-project\build\lib\cmake\mlir
-
-cmake --build build --target crossgl-opt -j 2
+cmake --build build --config Release --target crossgl-opt
 
 :: generates the LLVM Dialect -> print.ll 
 .\build\bin\crossgl-opt.exe .\test\Crossgl\print.mlir > build\bin\print.ll
