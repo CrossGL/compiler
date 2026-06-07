dff1b2227e0da16a8cd26bc0435dbcce1e3e6f59
Nripesh Niketan <nripesh14@gmail.com>
2026-06-07 20:49:55 +0100

    Cover unsupported switch boundaries

--- tests/cmake/CrossGLCheckTests.cmake
+++ tests/cmake/CrossGLCheckTests.cmake
@@ -1859,6 +1859,24 @@ crossgl_add_native_v0_unsupported_failure(
   7
   "message=restricted switch/case/default statements|message=native v0")
 crossgl_add_native_v0_unsupported_failure(
+  cglc_check_unsupported_native_v0_switch_grouped_labels_failure
+  ${CMAKE_CURRENT_SOURCE_DIR}/tests/check-failures/BadUnsupportedSwitchGroupedLabelsShader.cgl
+  7
+  7
+  "message=restricted switch/case/default statements|message=native v0")
+crossgl_add_native_v0_unsupported_failure(
+  cglc_check_unsupported_native_v0_switch_incompatible_case_label_type_failure
+  ${CMAKE_CURRENT_SOURCE_DIR}/tests/check-failures/BadUnsupportedSwitchIncompatibleCaseLabelTypeShader.cgl
+  7
+  7
+  "message=restricted switch/case/default statements|message=native v0")
+crossgl_add_native_v0_unsupported_failure(
+  cglc_check_unsupported_native_v0_switch_non_terminal_break_failure
+  ${CMAKE_CURRENT_SOURCE_DIR}/tests/check-failures/BadUnsupportedSwitchNonTerminalBreakShader.cgl
+  7
+  7
+  "message=restricted switch/case/default statements|message=native v0")
+crossgl_add_native_v0_unsupported_failure(
   cglc_check_unsupported_native_v0_for_in_failure
   ${CMAKE_CURRENT_SOURCE_DIR}/tests/check-failures/BadUnsupportedForInShader.cgl
   5
